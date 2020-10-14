4087054068b6a24f804a24ad6b4c4fa78fb80813
Igune Justine Robert <igunejustinerobert@gmail.com>
2020-10-14 16:35:56 +0300

    Modified files

--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className='product'>
       <div className='product__info'>
-        <p>The Lean Startup</p>
+        <p>{title}</p>
         <p className='product__price'>
-        <small>$</small>
-        <strong>24.99</strong>
+          <small>$</small>
+          <strong>{price}</strong>
         </p>
-        <div product__rating>
-        <p>🌟</p>
+        <div className='product__rating'>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
       </div>
-      </div>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg" alt="" />
+        <img src={image} alt='' />
 
         <button>Add to Basket</button>
     </div>
