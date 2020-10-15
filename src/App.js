a442d7f3b6cba7509909276c19ae1c2de559459c
Igune Justine Robert <igunejustinerobert@gmail.com>
2020-10-15 17:14:41 +0300

    a Build for Checkout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   return (
-     //BEM Naming convention for styling
+    <Router>
     <div className="app">
-      <Header/>
-      <Home />
+    <Header/>
+    <Switch>
+      <Route path="/checkout">
+        <Checkout />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
     </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className='header'>
-      <img
-       className='header__logo' src='https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_thumbnail_photos/001/244/731/datas/medium.png' alt='Soko' 
-      />
+      <Link to="/">
+        <img
+        className='header__logo' src='https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_thumbnail_photos/001/244/731/datas/medium.png' alt='Soko' 
+        />
+      </Link>
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
@@ -31,17 +34,15 @@ function Header() {
         <span className='header__optionLineTwo'>Prime</span>
       </div>
 
-      
+      <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
             <span className="header__optionLineTwo header__basketCount">
              0
             </span>
           </div>
-      
-      
+      </Link>
       </div> 
-
     </div> 
   )
 }
